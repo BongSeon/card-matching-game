6d4882c7b222c24849b93ce50138bea346983c79
Aestas <hawhawba@gmail.com>
2022-03-28 14:10:34 +0900

    update: card template

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,107 @@
 <template>
   <h1>title</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <Card
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`"
+      :value="card.value"
+      :position="card.position"
+      :matched="card.matched"
+      :visible="card.visible"
+      @select-card="cardClick"
+    />
   </section>
+  <div>
+    <h3>selPos</h3>
+    {{ selPos }}
+    <!-- <br />
+    {{ selPos.size }} -->
+    <h3>selVal</h3>
+    {{ selVal }}
+  </div>
 </template>
 
 <script>
-import Card from "./components/Card.vue";
+import Card from './components/Card.vue'
 
 export default {
-  name: "App",
   components: {
-    Card,
+    Card
   },
-};
+  data() {
+    return {
+      cardList: [],
+      selPos: new Set(), // user's select (max: 2)
+      selVal: new Set(), // user's select (max: 2)
+      checking: false // if user select 2, it's true while 1 sec
+    }
+  },
+  created() {
+    const cardItems = [
+      'bat',
+      'pumpkin',
+      'ghost',
+      'moon',
+      'cupcake',
+      'witch-hat',
+      'candy',
+      'cauldron'
+    ]
+    for (let i = 0; i < 16; i++) {
+      this.cardList.push({
+        value: cardItems[parseInt(i / 2)],
+        position: i,
+        matched: false,
+        visible: false
+      })
+    }
+  },
+  methods: {
+    async cardClick(position, value) {
+      if (this.checking == true) {
+        // 2개를 선택한 상황이고 오답일 경우 카드가 다시 뒤집어질때까지
+        // 클릭 막기 위한 조건문
+        return
+      }
+      console.log('flipCard:', position)
+      this.cardList[position].visible = !this.cardList[position].visible
+
+      this.selPos.add(position)
+      this.selVal.add(value)
+      if (this.selPos.size === 2) {
+        this.checking = true
+
+        // 2개 의 value가 같은지 체크하고
+        // 맞다면 matched 를 true
+        const match = this.selVal.size === 1
+        if (match) {
+          console.log('맞췄습니다. val:', value)
+          // this.cardList[this.selPos[0]].matched = true
+          // this.cardList[this.selPos[1]].matched = true
+          this.selPos.forEach((pos) => {
+            this.cardList[pos].matched = true
+          })
+        }
+
+        // this.selPos.forEach((sel) => {
+        //   console.log(sel)
+        // })
+
+        setTimeout(() => {
+          this.checking = false
+          if (!match) {
+            this.selPos.forEach((pos) => {
+              this.cardList[pos].visible = false
+            })
+          }
+          this.selPos.clear()
+          this.selVal.clear()
+        }, 1000)
+      }
+    },
+    selClear() {}
+  }
+}
 </script>
 
 <style>
@@ -40,17 +113,24 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-.card {
-  border: 5px solid #ccc;
-}
-
 .game-board {
   display: grid;
-  grid-template-columns: 100px 100px 100px 100px;
-  grid-template-rows: 100px 100px 100px 100px;
-  grid-column-gap: 30px;
-  grid-row-gap: 30px;
+  grid-template-columns: repeat(4, 60px);
+  grid-template-rows: repeat(4, 60px);
+  grid-column-gap: 12px;
+  grid-row-gap: 12px;
   justify-content: center;
+}
+@media screen and (min-width: 500px) {
+  .game-board {
+    grid-template-columns: repeat(4, 90px);
+    grid-template-rows: repeat(4, 90px);
+  }
+}
+@media screen and (min-width: 600px) {
+  .game-board {
+    grid-template-columns: repeat(4, 120px);
+    grid-template-rows: repeat(4, 120px);
+  }
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,86 @@
 <template>
-  <div>Card</div>
+  <div class="card" :class="{ 'is-flipped': !visible }" @click="selectCard">
+    <div class="card-face is-front">
+      <img :src="`/images/${value}.png`" :alt="value" />
+      <img v-if="matched" src="/images/checkmark.svg" alt="" />
+      <div>{{ position }} - {{ value }}</div>
+      <div v-if="matched">{{ matched }}</div>
+    </div>
+    <div class="card-face is-back">back</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
   props: {
-    msg: String,
+    value: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: Number,
+      required: true
+    },
+    matched: {
+      type: Boolean,
+      default: false
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
   },
-};
+  data() {
+    return {}
+  },
+  methods: {
+    selectCard() {
+      // console.log(
+      //   'val;',
+      //   this.value,
+      //   'pos;',
+      //   this.position,
+      //   'matched;',
+      //   this.matched,
+      //   'visible;',
+      //   this.visible
+      // )
+      if (!this.visible) {
+        this.$emit('select-card', this.position, this.value)
+      }
+    }
+  }
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.card {
+  cursor: pointer;
+  position: relative;
+  transition: transform 0.5s ease-in;
+  transform-style: preserve-3d;
+}
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+.card-face.is-front {
+  background-image: url('/images/card-bg.png');
+  color: #fff;
+  /* transform: rotateY(180deg); */
+}
+.card-face.is-back {
+  background-color: rgb(140, 197, 207);
+  transform: rotateY(180deg);
+}
+.card.is-flipped {
+  transform: rotateY(180deg);
+}
+</style>
